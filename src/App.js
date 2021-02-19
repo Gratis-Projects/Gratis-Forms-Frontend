f7af795ee3eda4c74a9528a45178d8f3d1a18051
Harsh Solanki <hsolanki1884@gmail.com>
2021-02-19 16:02:56 +0530

    global styles set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+    <h1>h1 heading</h1>
+    <h2>h2 heading</h2>
+    <h3>h3 heading</h3>
+    <h4>h4 heading</h4>
+    <p>paragraph</p>
+    </>
   );
 }
 
---
+++ src/globalStyles.js
@@ -0,0 +1,26 @@
+import {createGlobalStyle} from 'styled-components';
+
+
+const GlobalStyle=createGlobalStyle`
+body{
+    margin:0;
+    padding:0;
+    background:black;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-size:16px;
+    color:white;
+}
+h1{
+    font-size:1.8rem !important;
+}
+h2{
+    font-size:1.6rem !important;
+}
+h3{
+    font-size:1.4rem !important;
+}
+h4{
+    font-size:1.2rem !important;
+}`
+
+export default GlobalStyle;
